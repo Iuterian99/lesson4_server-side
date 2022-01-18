02bb031d08d42428868ff611b23b86b5d33774b8
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-19 00:35:15 +0500

    verifyMiddleware

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const cors = require("cors");
+const verifyMiddleware = require("./middlewares/verify")
+
+//!Middlewares
 app.use(express.json())
 app.use(cors())
 
+//!controllers
 const loginController = require("./controllers/login")
 const postsController = require("./controllers/posts")
 
 app.post("/login", loginController);
 
-app.get("/posts", postsController);
+app.get("/posts", verifyMiddleware, postsController);
 
 app.listen(port, () => {
   console.log(`running at localhost://${port}`);
