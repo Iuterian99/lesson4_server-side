b588c78be72f9d61138bc1d49533cd09a2fb81b1
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-18 23:51:51 +0500

    ..........

--- middlewares/verify.js
+++ middlewares/verify.js
@@ -1,4 +1,9 @@
 const { verifyUser } = require("../lib/jwt");
-module.exports =(req, res, next)= {
-
+module.exports =(req, res, next)=> {
+const { token } = req.headers;
+if(!token){
+  res.status(401).send({
+    message: 'Unothorized'
+  })
+}
 }
